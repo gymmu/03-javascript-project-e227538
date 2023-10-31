cab5e50d558435c537992db800a99ed6ef34256a
Sara <e227538@edu.sbl.ch>
2023-10-31 11:27:30 +0100

    Aufgabe 2 fertig und Aufgabe 3 angefangen

--- script.js
+++ script.js
@@ -13,11 +13,44 @@ export function aufgabe01(args) {
       result.push(currentElement)
     }
 
+    
+   
+  }
+  return result.join("")
+  }
 
 
+
+export function aufgabe02 (args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i] 
+  result.push (currentElement.toUpperCase())
+  }
   return result.join("")
 }
 
+
+export function aufgabe03 (args) {
+  const input = args
+  const result = [] // leere Liste wird erstellt
+  let count = 0
+  
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e") {  // wenn es e findet
+      count = count + 1
+    }
+
+    if (currentElement.toLowerCase() !== "e"){
+      result.push(currentElement)
+    }
+
+  }
+  return count
+}
 
 
 
