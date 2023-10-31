adf5a53c976a0421e4b68219282453594cedd3c8
Sara <e227538@edu.sbl.ch>
2023-10-31 10:31:09 +0100

    Augabe 1

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
-  }
+
+    if (currentElement === "e") {
+      // falls es ein e ist nicht an Liste hängen
+    }
+
+    if (currentElement.toLowerCase() !== "e"){
+      result.push(currentElement)
+    }
+
+
+
   return result.join("")
 }
+
+
+
+
